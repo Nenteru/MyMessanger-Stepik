b78f88cd2c40e878625534716f4eafdb5266dda0
Nenteru <it.requiem@gmail.com>
2023-08-18 15:34:14 +0300

    Добавлена серилизация и десерилизация

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Text.Json.Serialization;
 
 namespace MyMessanger_Stepik
 {
@@ -6,9 +8,14 @@ namespace MyMessanger_Stepik
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Start project");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("RusAl", "Privet", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializedMsg);
+            //{ "UserName":"RusAl","MessageText":"Privet","TimeStamp":"2023-08-18T12:32:58.2606102Z"}
+            //RusAl < 18.08.2023 12:32:58 >: Privet
+
         }
     }
 }
